eee4e3035bbfd70775d5730879bcd119b45bfd9b
Mathieu Lajoie <mlajoie03@gmail.com>
2021-05-07 10:27:11 -0400

    added some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,46 @@ export class AppComponent {
 
   showExt: boolean = false;
 
+  prefix: string = '';
+
+  suffix: string = '';
+
+  replace: string = '';
+
+  replaceWith: string = '';
+
   onFileChange(event: any): void {
-    this.fileNames = [...event.target.files].map((file: File) =>
-      this.showExt ? file.name : file.name.replace(/\.[^/.]+$/, "")
+    this.fileNames = [...event.target.files].map((file: File) => {
+      let ext = file.name.match(/\.[^/.]+$/);
+      let tempName = this.prefix + file.name.replace(/\.[^/.]+$/, "") + this.suffix;
+      if (this.showExt) {
+        tempName += ext ? ext[0] : '';
+      }
+      if (this.replace && this.replaceWith) {
+        tempName = tempName.replace(new RegExp(this.replace, 'g'), this.replaceWith)
+      }
+      return tempName;
+    }
     )
   }
 
   onExtChange(event: any): void {
     this.showExt = event.target.checked;
+  }
+
+  onPrefix(event: any): void {
+    this.prefix = event.target.value;
+  }
+
+  onSuffix(event: any): void {
+    this.suffix = event.target.value;
+  }
+
+  replaceFn(event: any): void {
+    this.replace = event.target.value;
+  }
+
+  replaceWithFn(event: any): void {
+    this.replaceWith = event.target.value;
   }
 }
